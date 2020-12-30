75328c1e2ae6dffbd677246182629dcce3f2541c
hodaya1 <48647966+hodaya1@users.noreply.github.com>
2020-12-30 20:23:26 +0200

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import getch
 import threading
 
 
-while 1:
+while 1:#so the program will run forever
 
     print("Client started, listening for offer requests...")
     while 1:
@@ -15,7 +15,7 @@ while 1:
             client.bind(("", 13117))
             data, addr = client.recvfrom(2048)
             client.close()
-            (cookie, mytype, port) = struct.unpack('!IBH', data)
+            (cookie, mytype, port) = struct.unpack('!IBH', data) #get the port from the broadcast message
             if cookie!=0xfeedbeef or mytype!=2:
                 raise Exception
             break
@@ -26,20 +26,15 @@ while 1:
 
     try:
         host = addr[0]
-        #BUFFER_SIZE = 2000 
-
-        #host = "172.1.0.41"       #TODO check
-        #port = 12000
-
         team_name = "shem_mekori"
         tcpClient = socket(AF_INET, SOCK_STREAM) 
         tcpClient.connect((host, port))
-        tcpClient.send(team_name.encode())
+        tcpClient.send(team_name.encode())#send message to the server with the group name
 
         global run_
         run_ = True
-        def press_keys(socket_tcp):
-            try:
+        def press_keys(socket_tcp): #this func send chars to the server
+            try: 
                 ten_seconds = time.time() + 10
                 while time.time()<ten_seconds: 
                     ch = getch.getch().encode()
@@ -51,12 +46,12 @@ while 1:
         data = tcpClient.recv(1024)
         print(data.decode())
 
-        game_th = threading.Thread(target=press_keys, args=(tcpClient,))
+        game_th = threading.Thread(target=press_keys, args=(tcpClient,)) #create thread for the game so just when the game start it will send chars
 
         game_th.start()
         
         try:
-            game_over = tcpClient.recv(1024)
+            game_over = tcpClient.recv(1024) #get a message about the result of the game
             run_ = False
             game_th.join()
             print(game_over.decode())
