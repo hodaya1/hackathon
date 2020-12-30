a75e10c3934a43a03a5407824356c03bb22c716b
hodaya1 <48647966+hodaya1@users.noreply.github.com>
2020-12-30 18:34:23 +0200

    Update client.py

--- client.py
+++ client.py
@@ -6,24 +6,31 @@ import threading
 
 
 while 1:
+
+    print("Client started, listening for offer requests...")
+    while 1:
+        try:     
+            client = socket(AF_INET, SOCK_DGRAM) # UDP
+            client.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+            client.bind(("", 13117))
+            data, addr = client.recvfrom(2048)
+            client.close()
+            (cookie, mytype, port) = struct.unpack('!IBH', data)
+            if cookie!=0xfeedbeef or mytype!=2:
+                raise Exception
+            break
+        except:
+            pass    
+    print( "Received offer from " +  addr[0] + " attempting to connect...")
+
     try:
-        print("Client started, listening for offer requests...")
-        client = socket(AF_INET, SOCK_DGRAM) # UDP
-        client.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        client.bind(("", 13117))
-        data, addr = client.recvfrom(2048)
-        server_port = struct.unpack('I B H', data)[2]
-        print( "Received offer from " +  addr[0] + " attempting to connect...")
+        host = addr[0]
+        #BUFFER_SIZE = 2000 
 
+        #host = "172.1.0.41"       #TODO check
+        #port = 12000
 
-        #host = addr[0] 
-        #port = server_port
-        BUFFER_SIZE = 2000 
-
-        host = "172.1.0.89"       #TODO check
-        port = 12000
-
-        team_name = "the_best_group"
+        team_name = "shem_mekori"
         tcpClient = socket(AF_INET, SOCK_STREAM) 
         tcpClient.connect((host, port))
         tcpClient.send(team_name.encode())
@@ -32,15 +39,17 @@ while 1:
         run_ = True
         def press_keys(socket_tcp):
             try:
-                while 1: 
+                ten_seconds = time.time() + 10
+                while time.time()<ten_seconds: 
                     ch = getch.getch().encode()
                     if run_:
+                        print("hi")
                         socket_tcp.send(ch)
             except:
                 pass
 
         data = tcpClient.recv(1024)
-        print(data)
+        print(data.decode())
 
         game_th = threading.Thread(target=press_keys, args=(tcpClient,))
 
@@ -50,7 +59,7 @@ while 1:
             game_over = tcpClient.recv(1024)
             run_ = False
             game_th.join()
-            print(game_over)
+            print(game_over.decode())
         except:
             run_ = False
             game_th.join()
@@ -58,8 +67,3 @@ while 1:
         print("server disconnected, listening for offer request...")
     except:
         pass
-
-
-
-
-
